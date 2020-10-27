07c0f4a94ed817a7c166b190dd3bed7a0a6977c8
Bona Fyrvall <bjorn.fyrvall@gmail.com>
2020-10-27 19:41:45 +0100

    Commit point before rework.

--- Assets/Scripts/NavMeshBoy.cs
+++ Assets/Scripts/NavMeshBoy.cs
@@ -6,10 +6,20 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class NavMeshBoy : MonoBehaviour
 {
+    public enum MovementType
+    {
+        None,
+        Patrol,
+        Random
+    }
+
     public float Speed = 5;
+    public MovementType Type;
 
     public List<Transform> PatrolPath;
     private int CurrentPatrolPathIndex;
+
+    public float MaxMovementDistance;
 
     private NavMeshAgent NavMeshAgent;
     private Agent Target;
@@ -22,9 +32,15 @@ public class NavMeshBoy : MonoBehaviour
 
     public MovementBase Movement;
 
+    private Vector3 CurrentMoveDirection;
+    private Vector3 CurrentLookDirection;
+
+    private Vector3 StartPosition;
+
     // Start is called before the first frame update
     private void Start()
     {
+        StartPosition = transform.position;
         NavMeshAgent = GetComponent<NavMeshAgent>();
         SetupNavMeshAgent(NavMeshAgent);
         Target = FindObjectOfType<Player>().GetComponent<Agent>();
@@ -37,36 +53,64 @@ public class NavMeshBoy : MonoBehaviour
     private void SetupNavMeshAgent(NavMeshAgent agent)
     {
         agent.updateRotation = false;
-        agent.updatePosition = false;
+        //agent.updatePosition = false;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (PatrolPath.Count == 0) {
-            NavMeshAgent.SetDestination(Target.transform.position);
-        } else {
-            NavMeshAgent.SetDestination(PatrolPath[CurrentPatrolPathIndex].position);
-            if(NavMeshAgent.pathStatus == NavMeshPathStatus.PathComplete && NavMeshAgent.remainingDistance < 1) {
-                CurrentPatrolPathIndex = (CurrentPatrolPathIndex + 1) % PatrolPath.Count;
-            }
+
+        if (Type == MovementType.Patrol) {
+            Patrol();
+        }else if(Type == MovementType.Random) {
+            MoveRandom();
         }
+        var nextMovePosition = NavMeshAgent.nextPosition;
+        var nextLookPosition = NavMeshAgent.pathEndPosition;
 
-        var direction = (NavMeshAgent.nextPosition - transform.position);
-        direction.y = 0;
-        direction.Normalize();
+        CurrentMoveDirection = (nextMovePosition - transform.position);
+        CurrentMoveDirection.y = 0;
+        CurrentMoveDirection.Normalize();
 
-        var angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        CurrentLookDirection = (nextLookPosition - transform.position);
+        CurrentLookDirection.y = 0;
+        CurrentLookDirection.Normalize();
 
-        TargetMovementDirection = direction;
+        var angle = Mathf.Atan2(CurrentLookDirection.x, CurrentLookDirection.z) * Mathf.Rad2Deg;
+
+        TargetMovementDirection = CurrentMoveDirection;
         CurrentMovementDirection = Movement.GetMovementDirection(CurrentMovementDirection, TargetMovementDirection);
         var offset = CurrentMovementDirection * Speed;
         transform.Translate(offset * Time.deltaTime, Space.World);
 
         CurrentRotationDirection = Movement.GetRotationDirection(CurrentRotationDirection, angle);
 
-        //transform.position = NavMeshAgent.nextPosition;
         transform.rotation = Quaternion.Euler(0, CurrentRotationDirection, 0);
+    }
 
+    private void Patrol()
+    {
+        if (PatrolPath.Count == 0) {
+            NavMeshAgent.SetDestination(Target.transform.position);
+        } else {
+            NavMeshAgent.SetDestination(PatrolPath[CurrentPatrolPathIndex].position);
+            if (NavMeshAgent.pathStatus == NavMeshPathStatus.PathComplete && NavMeshAgent.remainingDistance < 1) {
+                CurrentPatrolPathIndex = (CurrentPatrolPathIndex + 1) % PatrolPath.Count;
+            }
+        }
+    }
+
+    private void MoveRandom()
+    {
+        if (NavMeshAgent.pathStatus == NavMeshPathStatus.PathComplete && NavMeshAgent.remainingDistance < 1) {
+            var randomLength = Random.Range(0, MaxMovementDistance);
+            var randomAngle = Random.Range(0, Mathf.PI * 2);
+
+            var offset = new Vector3(Mathf.Sin(randomAngle), 0, Mathf.Cos(randomAngle)) * randomLength;
+
+            var target = StartPosition + offset;
+            NavMeshAgent.SetDestination(target);
+
+        }
     }
 }
